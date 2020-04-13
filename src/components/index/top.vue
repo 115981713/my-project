fec4e622bae0de7a678df2bbcb0c9ddb83c4655a
chitu <chitushgyl@163.com>
2020-04-13 19:12:09 +0800

    修改

---
+++ src/components/index/top.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="top">
+    <router-link to="/my">to my</router-link>
+    {{msg}}
+    <router-link to="/i">to I</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'top',
+  data () {
+    return {
+      msg: 'Welcome to My'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .top{
+    width:100%;
+    height:50px;
+    line-height:50px;
+    background-color:red;
+    text-align:center;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import My from '@/components/My'
+import I from '@/components/i'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/my',
       name: 'My',
       component: My
+    },    
+    {
+      path: '/i',
+      name: 'I',
+      component: I
     },
   ]
 })
